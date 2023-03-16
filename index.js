cd036c86d998607a34c77a68b90d705ac5a3cc17
rupokraihan <rupok2321@gmail.com>
2023-03-16 19:53:49 +0600

    Alhamdulillah Complete

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz, i) => {
-    console.log(quiz.options)
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
@@ -76,7 +75,7 @@ const displayQuiz = (data) => {
   
 };
 
-// EventListener for quiz submit button
+// EventListener for quiz submit button 
 document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
